53fd14beac4ccca889e8d9cfc7cb8c8f1b74a689
Jorrit Vander Mynsbrugge <44178217+jorritvm@users.noreply.github.com>
2024-04-06 15:49:50 +0200

    add bar chart

--- app/app.py
+++ app/app.py
@@ -8,9 +8,9 @@ from dotenv import load_dotenv
 import pandas as pd
 import dash
 from dash import dcc, html, Input, Output
-# import dash_leaflet as dl
-# from dash.dependencies import Input, Output
+import dash_leaflet as dl
 import dash_bootstrap_components as dbc
+import plotly.express as px
 
 # load data
 df = pd.read_parquet("../data/clean/gwpt.parquet")
@@ -61,45 +61,49 @@ time_slider = dcc.RangeSlider(
     min=y_min,
     max=y_max,
     step=1,
-    marks={i: str(i) for i in range(y_min, y_max+1) if i%10 == 0},
+    marks={i: str(i) for i in range(y_min, y_max+1) if i % 10 == 0},
     value=[y_min, y_max]  # Default value
 )
 
 # create the main map
-main_map = html.Div("main map content", id="temp", style={'background-color': 'green'})
+main_map = html.Div("main map content",
+                    id="main_map",
+                    style={'background-color': 'green'})
 
 # create the bar chart
-bar_chart = html.Div("bar chart content", style={'background-color': 'red'})
+bar_chart = html.Div("bar chart content",
+                     id="bar_chart",
+                     style={})
 
 # create the app's layout
 app.layout = dbc.Container([
     dbc.Row([html.H1("Global wind power tracker analysis",
                      className='text-center mb-4',
-                     style={'height': '25px'})]),
+                     style={'height': '45px'})]),
     dbc.Row([
-        dbc.Col( # sidebar column
+        dbc.Col(  # sidebar column
             [dbc.Row(x, style={'height': '15vh'}) for x in continents_dbc],
-            style={'background-color': 'lightblue', 'height': '100vh'},
+            style={'height': '100vh'},
             width=2),
         dbc.Col([
-            dbc.Row([ # filters row
+            dbc.Row([  # filters row
                 dbc.Col(sub_region_filter, width=2),
                 dbc.Col(country_filter, width=2),
                 dbc.Col(status_filter, width=2),
                 dbc.Col(time_slider, width=6)
                 ],
-                style={'background-color': 'lightblue', 'height': '5vh'}),
+                style={'height': '5vh'}),
             dbc.Row([
-                dbc.Col( # map column
+                dbc.Col(  # map column
                     dbc.Row(main_map, style={'height': '85vh'}),
-                    style={'background-color': 'lightyellow', 'height': '85vh'},
+                    style={'height': '85vh'},
                     width=9),
-                dbc.Col( # barchart column
+                dbc.Col(  # barchart column
                     dbc.Row(bar_chart, style={'height': '85vh'}),
-                    style={'background-color': 'pink', 'height': '85vh'},
+                    style={'height': '85vh'},
                     width=3),
                 ],
-                style={'background-color': 'aqua', 'height': '85vh'}),
+                style={'height': '85vh'}),
             ], width=10)
         ]),
     ], fluid=True
@@ -109,13 +113,19 @@ app.layout = dbc.Container([
 # callbacks
 @app.callback(
     [Output(f"{continent}_capacity", 'children') for continent in continents],
-    Input('status_filter', 'value')
+    [Input('status_filter', 'value'),
+     Input('time_slider', 'value')]
 )
-def update_capacities_on_cards(status):
+def update_capacities_on_cards(status, time_range):
     # filter by status
     tmp = agg.copy()
     if status is not None and status != []:
         tmp = agg[agg["Status"].isin(status)]
+
+    # filter by time range
+    if time_range is not None:
+        start_year, end_year = time_range
+        tmp = tmp[(tmp["Start year"] >= start_year) & (tmp["Start year"] <= end_year)]
 
     # make output values for every continent
     output_capacities = []
@@ -130,24 +140,25 @@ def update_capacities_on_cards(status):
     output_capacities = [round(x) for x in output_capacities]  # round
     output_capacities = [f"{x:,}" for x in output_capacities]  # put thousand sep ,
     output_capacities = [x.replace(",",".") for x in output_capacities]  # put thousand sep .
+    output_capacities = [f"{x} MW" for x in output_capacities]  # adding the SI unit .
     return output_capacities
+
 
 @app.callback(
     Output("sub_region_filter", 'options'),
     [Input(f"{continent}_click", 'n_clicks') for continent in continents]
 )
 def update_subregion_filter(*button_clicks):
+    options = []
     ctx = dash.callback_context
-    if not ctx.triggered:
-        button_id = None
-    else:
+    if ctx.triggered:
         button_id = ctx.triggered[0]['prop_id'].split('.')[0]
         continent = button_id.replace("_click", "")
         options = []
         if continent != "Total":
             options = agg[agg["Region"] == continent]["Subregion"].unique()
-
     return options
+
 
 @app.callback(
     Output("country_filter", "options"),
@@ -159,6 +170,34 @@ def update_country_filter(sub_region):
     else:
         countries = agg[agg["Subregion"] == sub_region]["Country"].unique()
         return countries
+
+
+@app.callback(
+    Output('bar_chart', 'children'),
+    [Input('status_filter', 'value'),
+     Input('time_slider', 'value')]
+)
+def update_bar_chart(status, time_range):
+    # Filter DataFrame based on status and time range
+    filtered_df = df.copy()
+    if status is not None and status != []:
+        filtered_df = filtered_df[filtered_df["Status"].isin(status)]
+    if time_range is not None:
+        start_year, end_year = time_range
+        filtered_df = filtered_df[(filtered_df["Start year"] >= start_year) & (filtered_df["Start year"] <= end_year)]
+
+    # Sort DataFrame by capacity in descending order and select top 20 wind farms
+    top_20 = filtered_df.nlargest(20, "Capacity (MW)")
+    top_20 = top_20[::-1]
+
+    # Create horizontal bar chart
+    fig = px.bar(top_20, x='Capacity (MW)', y='Project Name', orientation='h')
+    fig.update_layout(title_text="Top 20 Largest Wind Farms")
+
+    # Convert the Plotly figure to a Dash component
+    graph = dcc.Graph(figure=fig, style={'width': '100%', 'height': '100%'})
+
+    return graph
 
 
 if __name__ == "__main__":
